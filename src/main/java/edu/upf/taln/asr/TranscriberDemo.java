d64699425df34a0a7b2e89c3f1601688f86ce1f6
Juan Soler-Company <joan.soler.company@gmail.com>
2019-02-19 16:52:28 +0100

    updated main and pom

--- src/main/java/edu/upf/taln/asr/TranscriberDemo.java
+++ src/main/java/edu/upf/taln/asr/TranscriberDemo.java
@@ -6,15 +6,8 @@
 package edu.upf.taln.asr;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
-import edu.cmu.sphinx.api.Configuration;
-import edu.cmu.sphinx.api.SpeechResult;
-import edu.cmu.sphinx.api.StreamSpeechRecognizer;
-import edu.cmu.sphinx.decoder.adaptation.Stats;
-import edu.cmu.sphinx.decoder.adaptation.Transform;
-import edu.cmu.sphinx.result.WordResult;
+import java.nio.charset.Charset;
+import org.apache.commons.io.FileUtils;
 
 /**
  * A simple example that shows how to transcribe a continuous audio file that
@@ -27,12 +20,26 @@ public class TranscriberDemo {
         String LANG = "en";
         Transcriber t = new Transcriber(LANG);
         String pathDir = "C:\\Users\\U96153\\Documents\\NetBeansProjects\\Sphinx\\src\\main\\resources\\audios\\en";
-        
+        String resultDir = "C:\\Users\\U96153\\Documents\\NetBeansProjects\\Sphinx\\src\\main\\resources\\results\\en\\";
         File[] dir = new File(pathDir).listFiles();
         for(File f : dir)
         {
-            System.out.println(f.getAbsoluteFile().toString());
-            System.out.println(t.speechToText(f));
+            String name = f.getName();
+            int idF = Integer.parseInt(name.split("_")[1].split("\\.")[0]);
+            String spkInfo = name.split("_")[0];
+            String pathOut = resultDir+spkInfo+"_"+idF+".txt";
+            File outFile = new File(pathOut);
+
+            if(outFile.exists())
+            {
+                System.out.println(pathOut+"\nHave it, skipping");
+            }
+            else
+            {
+                String transcription = t.speechToText(f);
+                FileUtils.writeStringToFile(outFile, transcription, Charset.forName("UTF-8"));
+            }
+            
         }
     }
 }
